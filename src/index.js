2fc3798002f844c567bfead7621086e7cf922ca6
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2024-06-26 15:21:44 -0700

    Finish dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./style.css";
+const handleDropdownMenuClick = () => {
+  const options = document.querySelector(".options");
+  options.classList.toggle("visible");
+};
+
+const menu = document.querySelector(".menu");
+menu.addEventListener("click", handleDropdownMenuClick);
